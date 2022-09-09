3ac7318211e998be8fb964a89e31e72ffa281f4f
Yeison Cardona <yencardonaal@unal.edu.co>
2022-09-09 13:50:21 -0500

    Update with unal custom template

--- dimawebapp/wsgi.py
+++ dimawebapp/wsgi.py
@@ -8,16 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 import os
 import sys
 
-
-path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-sys.path.append(path)
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "dimawebapp.settings"
 application = get_wsgi_application()
-
-application = WhiteNoise(application, root=os.path.join(path, 'resources'))
-# application.add_files("/path/to/more/static/files", prefix="more-files/")
