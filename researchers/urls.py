775e1fd2e5042c8940ca7fc63c1c25268a08eb01
Yeison Cardona <yencardonaal@unal.edu.co>
2022-10-18 20:48:06 -0500

    Added percentages to bars

--- researchers/urls.py
+++ researchers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import Researchers
 urlpatterns = [
 
+    path('', Researchers.as_view(), name='researchers'),
     path('<slug:pk>', Researchers.as_view(), name='researchers'),
 
 ]
--- visualizations/views.py
+++ visualizations/views.py
@@ -16,7 +16,8 @@ def fix_filters(model, filters):
 
     for k in filters:
         k_plain = k.replace('~', '')
-        filters[k] = [c for c in model._meta.get_field(k_plain).choices if filters[k] in c][0][0]
+        filters[k] = [c for c in model._meta.get_field(
+            k_plain).choices if filters[k] in c][0][0]
 
     return filters
 
@@ -38,7 +39,8 @@ class BarsTemplatePlot(TemplateView):
             else:
                 plot = data['id'].split('--')[-1]
 
-            context.update(getattr(self, f'render_{plot}')(fix_filters(ResearchGroup, data['filters'])))
+            context.update(getattr(self, f'render_{plot}')(
+                fix_filters(ResearchGroup, data['filters'])))
             return self.render_to_response(context)
         except:
             return HttpResponseNotFound('<h1>Page not found</h1>')
@@ -49,14 +51,17 @@ class BarsTemplatePlot(TemplateView):
         self.template_name = "bars.html"
         if 'category' in filters:
             filters.pop('category')
-        x, y = zip(*[(ResearchGroup.objects.filter(ocde=key, **filters).count(), label) for key, label in ResearchGroup._meta.get_field('ocde').choices])
+        x, y = zip(*[(ResearchGroup.objects.filter(ocde=key, **filters).count(), label)
+                   for key, label in ResearchGroup._meta.get_field('ocde').choices])
         if sum(x) == 0:
             x, y = [], []
         else:
             x, y = map(list, (zip(*filter(lambda l: l[0], zip(x, y)))))
-            x = [round(100*xi/sum(x)) for xi in x]
-        bar_text = "%{x}%"
-        
+            percentages = [round(100 * xi / sum(x)) for xi in x]
+        texttemplate = "%{x}%"
+        hovertemplate = "%{text} grupos"
+        # 636EFA
+
         return locals()
 
     # ----------------------------------------------------------------------
@@ -65,28 +70,32 @@ class BarsTemplatePlot(TemplateView):
         self.template_name = "bars.html"
         if 'category' in filters:
             filters.pop('category')
-        x, y = zip(*[(ResearchGroup.objects.filter(knowledge_area=key, **filters).count(), label) for key, label in ResearchGroup._meta.get_field('knowledge_area').choices])
+        x, y = zip(*[(ResearchGroup.objects.filter(knowledge_area=key, **filters).count(), label)
+                   for key, label in ResearchGroup._meta.get_field('knowledge_area').choices])
         if sum(x) == 0:
             x, y = [], []
         else:
             x, y = map(list, (zip(*filter(lambda l: l[0], zip(x, y)))))
-            x = [round(100*xi/sum(x)) for xi in x]
-        bar_text = "%{x}%"
-        
+            percentages = [round(100 * xi / sum(x)) for xi in x]
+        texttemplate = "%{x}%"
+        hovertemplate = "%{text} grupos"
+
         return locals()
 
     # ----------------------------------------------------------------------
     def render_faculties(self, filters):
         """"""
         self.template_name = "bars.html"
-        x, y = zip(*[(ResearchGroup.objects.filter(faculty=key).count(), label) for key, label in ResearchGroup._meta.get_field('faculty').choices])
+        x, y = zip(*[(ResearchGroup.objects.filter(faculty=key).count(), label)
+                   for key, label in ResearchGroup._meta.get_field('faculty').choices])
         if sum(x) == 0:
             x, y = [], []
         else:
             x, y = map(list, (zip(*filter(lambda l: l[0], zip(x, y)))))
-            x = [round(100*xi/sum(x)) for xi in x]
-        bar_text = "%{x}%"
-        
+            percentages = [round(100 * xi / sum(x)) for xi in x]
+        texttemplate = "%{x}%"
+        hovertemplate = "%{text} grupos"
+
         return locals()
 
     # ----------------------------------------------------------------------
@@ -95,12 +104,15 @@ class BarsTemplatePlot(TemplateView):
         self.template_name = "pie.html"
         if 'category' in filters:
             filters.pop('category')
-        x, y = zip(*[(ResearchGroup.objects.filter(category=key, **filters).count(), label) for key, label in ResearchGroup._meta.get_field('category').choices])
+        x, y = zip(*[(ResearchGroup.objects.filter(category=key, **filters).count(), label)
+                   for key, label in ResearchGroup._meta.get_field('category').choices])
         if sum(x) == 0:
             x, y = [], []
         else:
             x, y = map(list, (zip(*filter(lambda l: l[0], zip(x, y)))))
             #x = [round(100*xi/sum(x)) for xi in x]
+
+        hovertemplate = "%{value} grupos"
 
         return locals()
 
@@ -123,6 +135,7 @@ class GenerateFilteredOptionsView(View):
         departaments = set([d[0] for d in ResearchGroup.objects.filter(**{k: filters[k]
                                                                           for k in ['faculty'] if k in
                                                                           filters}).values_list('departament')])
-        data['departaments'] = [dict(ResearchGroup._meta.get_field('departament').choices)[d] for d in departaments]
+        data['departaments'] = [dict(ResearchGroup._meta.get_field(
+            'departament').choices)[d] for d in departaments]
 
         return HttpResponse(json.dumps(data), content_type='text/json')
